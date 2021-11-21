7ab75db5aed0036447dcfbc647abf17038eaff99
Dehong <1252825428@qq.com>
2021-11-21 14:39:30 +0800

    leetcode 1120

---
+++ src/simple/T559_N叉树的最大深度.java
@@ -0,0 +1,32 @@
+package simple;
+
+import java.util.List;
+
+public class T559_N叉树的最大深度 {
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int max = 0;
+        for (Node node : root.children) {
+            max = Math.max(max, maxDepth(node));
+        }
+        return max + 1;
+    }
+}
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
